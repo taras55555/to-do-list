0073a442298b1e2cfb7b5388ba809272c328bfd7
Тарас Камінський <tkaminskyi@gmail.com>
2024-09-02 20:25:19 +0300

    Fixed localStorage initialization issue

--- src/contexts/ToDoListContext.jsx
+++ src/contexts/ToDoListContext.jsx
@@ -3,10 +3,18 @@ import React, { useState, createContext, useContext, useEffect } from "react";
 const ToDoListContext = createContext();
 
 export function ToDoListProvider({ children }) {
-    const [toDoList, setToDoList] = useState(JSON.parse(localStorage.getItem('ToDoList')));
+
+    const [toDoList, setToDoList] = useState(JSON.parse(localStorage.getItem('ToDoList')) || []);
     const [toggleNewTaskButton, setToggleNewTaskButton] = useState(true);
 
-    useEffect(() => localStorage.setItem('ToDoList', JSON.stringify(toDoList)), [toDoList])
+    useEffect(() => {
+        document.title = `To-Do List`
+    }, [])
+
+    useEffect(() => {
+        console.log(JSON.parse(localStorage.getItem('ToDoList')))
+        localStorage.setItem('ToDoList', JSON.stringify(toDoList))
+    }, [toDoList])
 
     return (
         <ToDoListContext.Provider value={{ toDoList, setToDoList, toggleNewTaskButton, setToggleNewTaskButton }}>
